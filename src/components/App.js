ef43520e66f4a6eb1169e47ab7fe1f5ac9e81a4b
kgaolodevs <kgaolodev@gmail.com>
2020-08-12 18:50:00 +0200

    Created base styling and base App component

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import "../css/main.css";
+
+class App extends Component {
+  state = {};
+  render() {
+    return <div>Hello App!</div>;
+  }
+}
+
+export default App;
