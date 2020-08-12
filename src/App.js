4a478a4868152ab680e0770717572d8af90d3fe1
kgaolodevs <kgaolodev@gmail.com>
2020-08-12 22:46:36 +0200

    Handled API response

--- src/App.js
+++ src/App.js
@@ -8,21 +8,29 @@ const data = {
 };
 
 class App extends Component {
+  state = { videos: [] };
+
   onTermSubmit = async (term) => {
-    axios.get("https://www.googleapis.com/youtube/v3/search", {
-      params: {
-        part: "snippet",
-        maxResult: 5,
-        key: data.KEY,
-        q: term,
-      },
-    });
+    const response = await axios.get(
+      "https://www.googleapis.com/youtube/v3/search",
+      {
+        params: {
+          part: "snippet",
+          maxResult: 5,
+          key: data.KEY,
+          q: term,
+        },
+      }
+    );
+
+    this.setState({ videos: response.data.items });
   };
 
   render() {
     return (
       <div className="ui container app">
         <SearchBar onFormSubmit={this.onTermSubmit} />
+        {/* Videos found: {this.state.videos.length} */}
       </div>
     );
   }
