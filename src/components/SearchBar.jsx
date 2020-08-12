8d9daefffb87194c27edb780150d092a5c2610c4
kgaolodevs <kgaolodev@gmail.com>
2020-08-12 21:28:30 +0200

    Added submit button

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,16 +1,35 @@
-import React from "react";
+import React, { Component } from "react";
 
-const SearchBar = () => {
-  return (
-    <div className="search-bar ui segment" id="app__searchBar">
-      <form className="ui form">
-        <div className="field">
-          <label id="app__searchBar--title">SEARCH FOR A VIDEO</label>
-          <input type="text" />
-        </div>
-      </form>
-    </div>
-  );
-};
+class SearchBar extends Component {
+  state = { searchTerm: "" };
+
+  onInputChange = (e) => {
+    this.setState({ searchTerm: e.target.value });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  render() {
+    return (
+      <div className="search-bar ui segment" id="app__searchBar">
+        <form onSubmit={this.handleSubmit} className="ui form">
+          <div className="field">
+            <label id="app__searchBar--title">SEARCH FOR A VIDEO</label>
+            <div id="app__searchBar--content">
+              <input
+                type="text"
+                value={this.state.searchTerm}
+                onChange={this.onInputChange}
+              />
+              <button className="ui violet button ">Search</button>
+            </div>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
 
 export default SearchBar;
