efee0ac514d6c60f631c07223b0d52fe4be91246
kgaolodevs <kgaolodev@gmail.com>
2020-08-13 13:18:55 +0200

    Added iframe to VideoDetail component

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -4,7 +4,20 @@ const VideoDetail = ({ video }) => {
   if (!video) {
     return <div>Loading...</div>;
   }
-  return <div>{video.snippet.title}</div>;
+
+  const videoSource = `https://www.youtube.com/embed/${video.id.videoId}`;
+
+  return (
+    <div>
+      <div className="ui embed">
+        <iframe src={videoSource} />
+      </div>
+      <div className="ui segment">
+        <h3 className="ui header">{video.snippet.title}</h3>
+        <p>{video.snippet.description}</p>
+      </div>
+    </div>
+  );
 };
 
 export default VideoDetail;
